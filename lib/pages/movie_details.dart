d86d12531344af014e810b8c03d5f3b3b6771fa0
Enes Çakmak <cakmakkeness@gmail.com>
2024-07-31 15:21:20 +0300

    build files

---
+++ lib/pages/movie_details.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/models/movie.dart';
+
+class MovieDetails extends StatelessWidget {
+  const MovieDetails({super.key, required this.movie});
+  final Movie movie;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(movie.title)),
+    );
+  }
+}
---
+++ lib/providers/movie_provider.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/models/movie.dart';
+import 'package:movie_app/utils/movie_parser.dart';
+
+class MovieProvider extends ChangeNotifier {
+  List<Movie> _movieList = [
+    // "The Shawsank Redemption",
+    // "The Godfather",
+    // "The Dark Knight",
+    // "The Godfather: Part II",
+    // "12 Angry",
+  ];
+
+  List<Movie> get movieList => _movieList;
+
+  Future<void> loadMovies(BuildContext context) async {
+    try {
+      final jsonString = await DefaultAssetBundle.of(context)
+          .loadString('assets/data/film.json');
+      final movies = MovieParser.parse(jsonString);
+      _movieList = movies;
+      notifyListeners();
+    } catch (e) {
+      print("Error loading movies: $e");
+    }
+  }
+}
