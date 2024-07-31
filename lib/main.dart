b1e1159f2d74892efc3e2b8b40dbdef29fb5bcf7
Enes Çakmak <cakmakkeness@gmail.com>
2024-07-31 15:26:46 +0300

    feat: seperate movie_card for order

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app/components/movie_card.dart';
 import 'package:movie_app/models/movie.dart';
 import 'package:movie_app/pages/movie_details.dart';
 import 'package:movie_app/providers/movie_provider.dart';
@@ -65,64 +66,5 @@ class _HomeState extends State<Home> {
                 // return Card(child: Center(child: Text(movies[index])));
               }),
         ));
-  }
-}
-
-class MovieCard extends StatelessWidget {
-  const MovieCard({
-    super.key,
-    required this.movie,
-  });
-
-  final Movie movie;
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-        child: ExpansionTile(
-      title: Text(movie.title),
-      subtitle: Text(movie.director),
-      leading: CircleAvatar(child: Text(movie.title[0])),
-      children: [
-        Container(
-            alignment: Alignment.center,
-            padding: EdgeInsets.only(
-              left: 75,
-            ),
-            child: Column(
-              children: [
-                RichText(
-                    text: TextSpan(
-                        style: DefaultTextStyle.of(context).style,
-                        children: [
-                      TextSpan(
-                          text: 'Released: ',
-                          style: Theme.of(context)
-                              .textTheme
-                              .labelLarge
-                              ?.copyWith(fontWeight: FontWeight.bold)),
-                      TextSpan(text: '${movie.released} \n'),
-                      TextSpan(
-                          text: 'Plot: ',
-                          style: Theme.of(context)
-                              .textTheme
-                              .labelLarge
-                              ?.copyWith(fontWeight: FontWeight.bold)),
-                      TextSpan(text: '${movie.plot} '),
-                    ])),
-                TextButton(
-                    onPressed: () {
-                      // Movie Details
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) =>
-                                  MovieDetails(movie: movie)));
-                    },
-                    child: Text('Read More'))
-              ],
-            ))
-      ],
-    ));
   }
 }
